6f4ab419cd71f720d30579701c0d01e4641bf47d
Alex Leventer <alex.leventer@datastax.com>
2022-01-06 16:43:35 -0800

    Convert cardheader and announcement to tsx

--- src/components/CardHeader/CardHeader.tsx
+++ src/components/CardHeader/CardHeader.tsx
@@ -1,8 +1,14 @@
 import { Box, Typography } from "@material-ui/core";
-// import "./cardheader.scss";
+import React from "react";
+import { Theme } from "react-query/types/devtools/theme";
+
+interface CardHeaderProps {
+  title: Theme;
+  children?: React.ReactNode;
+}
 
 // (aphex) removed children arg in favor of spread to silence compiler while refactoring parents
-const CardHeader = ({ title, ...props }) => {
+const CardHeader = ({ title, ...props }: CardHeaderProps) => {
   return (
     <Box className={`card-header`}>
       <Typography variant="h5">{title}</Typography>
